f600df47f7797538da14f72a049b739a6d8fd540
MD. TAZBID SIKDER <u2308012@student.cuet.ac.bd>
2026-07-19 11:05:13 +0600

    Update index.ts

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,5 +1,14 @@
 import { skillGroup, skillHighlights } from './skills';
 import overview from './overview';
 import siteSettings from './siteSettings';
+import photographySingle from './photographySingle';
+import photographyStory from './photographyStory';
 
-export const schemaTypes = [skillGroup, skillHighlights, overview, siteSettings];
+export const schemaTypes = [
+  skillGroup, 
+  skillHighlights, 
+  overview, 
+  siteSettings, 
+  photographySingle, 
+  photographyStory
+];
